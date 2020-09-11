7726cceae87403ea44014d1c99b6e7be0469d328
dhintz <dhintz89@gmail.com>
2020-09-10 22:15:33 -0400

    add main layout view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
@@ -23,17 +24,21 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li>
+                            <a href="{{ url('/') }}">Home</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -54,7 +59,26 @@
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <!-- new logout code -->
+                                <ul class="dropdown-menu" role="menu">
+                                    @if (Auth::user()->can_post())
+                                    <li>
+                                        <a href="{{ url('/new-post') }}">Add new post</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ url('/user/'.Auth::id().'/posts') }}">My Posts</a>
+                                    </li>
+                                    @endif
+                                    <li>
+                                        <a href="{{ url('/user/'.Auth::id()) }}">My Profile</a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ url('/auth/logout') }}">Logout</a>
+                                    </li>
+                                </ul>
+
+                                <!-- original logout code -->
+                                <!-- <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -63,7 +87,7 @@
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
-                                    </form>
+                                    </form> -->
                                 </div>
                             </li>
                         @endguest
@@ -72,9 +96,43 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container">
+            @if (Session::has('message'))
+            <div class="flash alert-info">
+                <p class="panel-body">
+                    {{ Session::get('message') }}
+                </p>
+            </div>
+            @endif
+            @if ($errors->any())
+            <div class='flash alert-danger'>
+                <ul class="panel-body">
+                    @foreach ( $errors->all() as $error )
+                    <li>
+                        {{ $error }}
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h2>@yield('title')</h2>
+                            @yield('title-meta')
+                        </div>
+                        <div class="panel-body">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <!-- Scripts -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
